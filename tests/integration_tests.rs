ecb5fe83d81b3e51883a13dbcdad703187a0612a
Drew Silcock <drew.silcock@stfc.ac.uk>
2021-10-26 17:27:01 +0100

    Add test for 1999 sample COMTRADE file.

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -204,7 +204,7 @@ fn it_correctly_parses_sample_2013_files_with_ascii_data() {
 }
 
 #[test]
-fn it_correctly_parses_sample_2013_files_with_binary_data() {
+fn it_correctly_parses_sample_2013_files_with_binary16_data() {
     let dir = Path::new(SAMPLE_COMTRADE_DIR);
     let cfg_path = dir.join("sample_2013_bin.cfg");
     let dat_path = dir.join("sample_2013_bin.dat");
@@ -817,6 +817,248 @@ fn it_correctly_parses_sample_2013_files_with_ascii_data_using_utf8() {
                     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
                 ],
+            },
+        ],
+    };
+
+    assert_eq!(record, expected);
+}
+
+#[test]
+fn it_correctly_parses_sample_1999_files_with_binary16_data() {
+    let dir = Path::new(SAMPLE_COMTRADE_DIR);
+    let cfg_path = dir.join("sample_1999_bin.cfg");
+    let dat_path = dir.join("sample_1999_bin.dat");
+
+    let cfg_file = BufReader::new(File::open(cfg_path).expect("unable to find sample cfg file"));
+    let dat_file = BufReader::new(File::open(dat_path).expect("unable to find sample dat file"));
+
+    let record = ComtradeParserBuilder::new()
+        .cfg_file(cfg_file)
+        .dat_file(dat_file)
+        .build()
+        .parse()
+        .expect("unable to parse COMTRADE files");
+
+    let expected = Comtrade {
+        station_name: "station".to_string(),
+        recording_device_id: "equipment".to_string(),
+        revision: FormatRevision::Revision1999,
+        line_frequency: 60.0,
+        sampling_rates: vec![SamplingRate {
+            rate_hz: 15360.0,
+            end_sample_number: 5,
+        }],
+        start_time: NaiveDate::from_ymd(2017, 01, 07).and_hms_micro(15, 35, 41, 958_268),
+        trigger_time: NaiveDate::from_ymd(2017, 01, 07).and_hms_micro(15, 35, 41, 958_333),
+        data_format: DataFormat::Binary16,
+        timestamp_multiplication_factor: 1.0,
+        time_offset: None,
+        local_offset: None,
+        time_quality: None,
+        leap_second_status: None,
+        num_total_channels: 20,
+        num_analog_channels: 4,
+        num_status_channels: 16,
+
+        sample_numbers: vec![],
+        timestamps: vec![],
+
+        analog_channels: vec![
+            AnalogChannel {
+                index: 1,
+                name: "VA".to_string(),
+                phase: "A".to_string(),
+                circuit_component_being_monitored: "obj".to_string(),
+                units: "kV".to_string(),
+                min_value: -32767.0,
+                max_value: 32767.0,
+                multiplier: 0.000361849,
+                offset_adder: 0.0,
+                skew: 0.0,
+                primary_factor: 120.0,
+                secondary_factor: 1.0,
+                scaling_mode: AnalogScalingMode::Primary,
+                data: vec![],
+            },
+            AnalogChannel {
+                index: 2,
+                name: "VB".to_string(),
+                phase: "B".to_string(),
+                circuit_component_being_monitored: "obj".to_string(),
+                units: "kV".to_string(),
+                min_value: -32767.0,
+                max_value: 32767.0,
+                multiplier: 0.000365758,
+                offset_adder: 0.0,
+                skew: 0.0,
+                primary_factor: 120.0,
+                secondary_factor: 1.0,
+                scaling_mode: AnalogScalingMode::Primary,
+                data: vec![],
+            },
+            AnalogChannel {
+                index: 3,
+                name: "VC".to_string(),
+                phase: "C".to_string(),
+                circuit_component_being_monitored: "obj".to_string(),
+                units: "kV".to_string(),
+                min_value: -32767.0,
+                max_value: 32767.0,
+                multiplier: 0.000371569,
+                offset_adder: 0.0,
+                skew: 0.0,
+                primary_factor: 120.0,
+                secondary_factor: 1.0,
+                scaling_mode: AnalogScalingMode::Primary,
+                data: vec![],
+            },
+            AnalogChannel {
+                index: 4,
+                name: "VN".to_string(),
+                phase: "N".to_string(),
+                circuit_component_being_monitored: "obj".to_string(),
+                units: "kV".to_string(),
+                min_value: -32767.0,
+                max_value: 32767.0,
+                multiplier: 0.000016493,
+                offset_adder: 0.0,
+                skew: 0.0,
+                primary_factor: 60.0,
+                secondary_factor: 1.0,
+                scaling_mode: AnalogScalingMode::Primary,
+                data: vec![],
+            },
+        ],
+
+        status_channels: vec![
+            StatusChannel {
+                index: 1,
+                name: "ST_1".to_string(),
+                phase: "".to_string(),
+                circuit_component_being_monitored: "".to_string(),
+                normal_status_value: 0,
+                data: vec![],
+            },
+            StatusChannel {
+                index: 2,
+                name: "ST_2".to_string(),
+                phase: "".to_string(),
+                circuit_component_being_monitored: "".to_string(),
+                normal_status_value: 0,
+                data: vec![],
+            },
+            StatusChannel {
+                index: 3,
+                name: "ST_3".to_string(),
+                phase: "".to_string(),
+                circuit_component_being_monitored: "".to_string(),
+                normal_status_value: 0,
+                data: vec![],
+            },
+            StatusChannel {
+                index: 4,
+                name: "ST_4".to_string(),
+                phase: "".to_string(),
+                circuit_component_being_monitored: "".to_string(),
+                normal_status_value: 0,
+                data: vec![],
+            },
+            StatusChannel {
+                index: 5,
+                name: "ST_5".to_string(),
+                phase: "".to_string(),
+                circuit_component_being_monitored: "".to_string(),
+                normal_status_value: 0,
+                data: vec![],
+            },
+            StatusChannel {
+                index: 6,
+                name: "ST_6".to_string(),
+                phase: "".to_string(),
+                circuit_component_being_monitored: "".to_string(),
+                normal_status_value: 0,
+                data: vec![],
+            },
+            StatusChannel {
+                index: 7,
+                name: "ST_7".to_string(),
+                phase: "".to_string(),
+                circuit_component_being_monitored: "".to_string(),
+                normal_status_value: 0,
+                data: vec![],
+            },
+            StatusChannel {
+                index: 8,
+                name: "ST_8".to_string(),
+                phase: "".to_string(),
+                circuit_component_being_monitored: "".to_string(),
+                normal_status_value: 0,
+                data: vec![],
+            },
+            StatusChannel {
+                index: 9,
+                name: "ST_9".to_string(),
+                phase: "".to_string(),
+                circuit_component_being_monitored: "".to_string(),
+                normal_status_value: 0,
+                data: vec![],
+            },
+            StatusChannel {
+                index: 10,
+                name: "ST_10".to_string(),
+                phase: "".to_string(),
+                circuit_component_being_monitored: "".to_string(),
+                normal_status_value: 0,
+                data: vec![],
+            },
+            StatusChannel {
+                index: 11,
+                name: "ST_11".to_string(),
+                phase: "".to_string(),
+                circuit_component_being_monitored: "".to_string(),
+                normal_status_value: 0,
+                data: vec![],
+            },
+            StatusChannel {
+                index: 12,
+                name: "ST_12".to_string(),
+                phase: "".to_string(),
+                circuit_component_being_monitored: "".to_string(),
+                normal_status_value: 0,
+                data: vec![],
+            },
+            StatusChannel {
+                index: 13,
+                name: "ST_13".to_string(),
+                phase: "".to_string(),
+                circuit_component_being_monitored: "".to_string(),
+                normal_status_value: 0,
+                data: vec![],
+            },
+            StatusChannel {
+                index: 14,
+                name: "ST_14".to_string(),
+                phase: "".to_string(),
+                circuit_component_being_monitored: "".to_string(),
+                normal_status_value: 0,
+                data: vec![],
+            },
+            StatusChannel {
+                index: 15,
+                name: "ST_15".to_string(),
+                phase: "".to_string(),
+                circuit_component_being_monitored: "".to_string(),
+                normal_status_value: 0,
+                data: vec![],
+            },
+            StatusChannel {
+                index: 16,
+                name: "ST_16".to_string(),
+                phase: "".to_string(),
+                circuit_component_being_monitored: "".to_string(),
+                normal_status_value: 0,
+                data: vec![],
             },
         ],
     };
